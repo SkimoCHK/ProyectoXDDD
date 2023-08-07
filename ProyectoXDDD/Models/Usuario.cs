76d3ca83706862eb00d877d22988084604ab83b2
isabelfloresl <isabel.flumbrera@gmail.com>
2023-08-07 08:00:38 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoXDDD/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoXDDD.Models
+{
+    public class Usuario
+    {
+        public int IDUSUARIO { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Contrasenia { get; set; }
+
+
+
+    }
+}
